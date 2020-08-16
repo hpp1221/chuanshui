881a3c3d4cfaf4b5187fa69889318086d047f4cf
hpp <981577920@qq.com>
2020-08-16 23:17:59 +0800

    角色管理模块部分完成

--- src/components/page/system-settings/role-management/OperationRoleModule.vue
+++ src/components/page/system-settings/role-management/OperationRoleModule.vue
@@ -1,68 +1,153 @@
 <template>
     <div>
-        <div class="container clearfix">
-            <div class="handle-box">
-              <div class="global-table-title">
-                <div class="title">
-                    <i></i>
-                    <span>权限管理</span>
-                </div>
-                <el-button type="primary" @click="handleCreateAuthority">新建权限</el-button>
-            </div>
-            </div>
-        </div>
-        <div class="container m-t-16 p-t-0">
+        <div class="role-content">
             <div class="global-table-title">
                 <div class="title">
                     <i></i>
-                    <span>权限管理</span>
+                    <span>角色信息</span>
                 </div>
-                <el-button type="primary" @click="handleCreateAuthority">新建权限</el-button>
             </div>
-            <el-table
-                :data="tableData"
-                v-loading="loading"
-                row-key="id"
-                :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
+            <el-form
+                ref="roleForm"
+                :model="roleForm"
+                label-width="80px"
+                :inline="true"
+                class="role-form"
+                :rules="roleRules"
             >
-                <el-table-column prop="authorityName" label="权限名称">
-                    <template slot-scope="scope">
-                        <span v-if="!scope.row.children" class="child-line">|</span>
-                        <span
-                            v-if="!scope.row.children"
-                            class="child-line"
-                            style="marginRight:10px"
-                        >————————</span>
-                        <span>{{scope.row.authorityName}}</span>
-                    </template>
-                </el-table-column>
-                <el-table-column prop="name" label="别名"></el-table-column>
-                <el-table-column prop="router" label="路由"></el-table-column>
-                <el-table-column label="操作" width="180">
-                    <template slot-scope="scope">
-                        <el-button
-                            type="text"
-                            v-if="scope.row.children"
-                            @click="handleCreateChildNode(scope.$index, scope.row)"
-                        >新建下级</el-button>
-                        <el-button type="text" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                        <el-button
-                            type="text"
-                            class="delete-color"
-                            @click="handleDelete(scope.$index, scope.row)"
-                        >删除</el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
+                <el-form-item label="角色名称" prop="name">
+                    <el-input v-model="roleForm.name"></el-input>
+                </el-form-item>
+                <el-form-item label="描述" prop="address">
+                    <el-input v-model="roleForm.address"></el-input>
+                </el-form-item>
+            </el-form>
+        </div>
+        <div class="role-content">
+            <div class="global-table-title">
+                <div class="title">
+                    <i></i>
+                    <span>权限分配</span>
+                </div>
+            </div>
+            <Vue2OrgTree name="test" :data="data" :render-content="renderContent"/>
         </div>
     </div>
 </template>
 <script>
+import Vue from 'vue';
+import Vue2OrgTree from 'vue2-org-tree';
+import 'vue2-org-tree/dist/style.css';
+Vue.use(Vue2OrgTree);
+import './RoleManagement.less';
+import { Divider } from 'element-ui';
 export default {
     name: 'OperationRoleModule',
     data() {
-        return {};
+        return {
+            roleForm: {},
+            roleRules: {
+                name: [
+                    {
+                        required: true,
+                        message: '请输入'
+                    }
+                ],
+                address: [
+                    {
+                        required: true,
+                        message: '请输入'
+                    }
+                ]
+            },
+            data: {
+                id: 0,
+                label: 'XXX科技有限公司',
+                children: [
+                    {
+                        id: 2,
+                        label: '产品研发部',
+                        children: [
+                            {
+                                id: 5,
+                                label: '研发-前端'
+                            },
+                            {
+                                id: 6,
+                                label: '研发-后端'
+                            },
+                            {
+                                id: 9,
+                                label: 'UI设计'
+                            },
+                            {
+                                id: 10,
+                                label: '产品经理'
+                            }
+                        ]
+                    },
+                    {
+                        id: 3,
+                        label: '销售部',
+                        checked:true,
+                        children: [
+                            {
+                                id: 7,
+                                label: '销售一部'
+                            },
+                            {
+                                id: 8,
+                                label: '销售二部'
+                            }
+                        ]
+                    },
+                    {
+                        id: 4,
+                        label: '财务部'
+                    },
+                    {
+                        id: 9,
+                        label: 'HR人事'
+                    }
+                ]
+            },
+            horizontal: false,
+            collapsable: true,
+            expandAll: false,
+            labelClassName: 'bg-white'
+        };
     },
-    methods: {}
+    methods: {
+        renderContent(h, data) {
+            console.log('hhh',data.label,data);
+            return <div><el-checkbox style="margin:0 8px 5px 0" v-model={data.checked}></el-checkbox><span>{data.label}</span></div>;
+        }
+    }
 };
 </script>
+<style type="text/css">
+.org-tree-node-label {
+    white-space: nowrap;
+}
+.bg-white {
+    background-color: white;
+}
+.bg-orange {
+    background-color: orange;
+}
+.bg-gold {
+    background-color: gold;
+}
+.bg-gray {
+    background-color: gray;
+}
+.bg-lightpink {
+    background-color: lightpink;
+}
+.bg-chocolate {
+    background-color: chocolate;
+}
+.bg-tomato {
+    background-color: tomato;
+}
+</style>
